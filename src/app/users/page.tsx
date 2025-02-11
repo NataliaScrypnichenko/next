717d23247d45a4c6cfc4d438a4b6538168331749
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-02-11 23:19:36 +0200

    3.3 ISR-готов

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,11 +1,13 @@
 import {IUser} from "@/app/model/IUser";
-
+// {next: {revalidate: 5}}- це задоно час на обновлення даних=революдувати сторінку?коли пишемо там 0 то сторінку перетворює
 const UsersPage = async () => {
-    const users = await fetch("http://jsonplaceholder.typicode.com/users")
+    const users = await fetch("http://jsonplaceholder.typicode.com/users", {next: {revalidate: 2}})//{cache:'no-cache'}
         .then((value) =>value .json());
 
     return (
         <div>
+            {/*повертає значення в мл секундах значення,щоб виконати команду потрібно (run start)*/}
+            <h2>{Date.now()}</h2>
             {
                 users.map((user:IUser) => (<div key={user.id}>
                     {user.username}
