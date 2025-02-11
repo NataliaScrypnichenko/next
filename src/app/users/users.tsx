aa320ae5da428e6156e5f93b7ac59fca5bfa616a
NataliaScrypnichenko <cornanatalia604@gmail.com>
2025-02-11 20:50:36 +0200

    3.1 SSG дає помилку

--- src/app/users/users.tsx
+++ src/app/users/users.tsx
@@ -1,13 +1,12 @@
-import {IUser} from "@/app/model/IUser";
-
 
 const UsersPage = async () => {
-    const users = await fetch('http://jsonplaceholder.typicode.com/users')
+    const users = await fetch("http://jsonplaceholder.typicode.com/users")
         .then((value) =>value .json());
 
     return (
         <div>
-            {users.map((user:IUser) => (<div key={user.id}>{user.name}</div>))}
+            {users.map((user: {id:number,name:string }) =>
+                (<div key={user.id}>{user.name}</div>))}
         </div>
     );
 };
